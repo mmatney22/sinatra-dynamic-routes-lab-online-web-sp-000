e2973b484682db250f355ece33dc3bb483168bd3
Morgan Matney <morgan.matney89@gmail.com>
2020-11-17 13:07:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number]
     @phrase = params[:phrase]
-    "#{phrase.}"
+    @phrase.each {|p| p
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
