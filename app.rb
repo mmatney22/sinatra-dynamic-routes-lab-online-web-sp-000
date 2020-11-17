7e716fb24c3e7f65fef260ee1684ef3bcecfa28e
Morgan Matney <morgan.matney89@gmail.com>
2020-11-17 13:18:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,7 @@ class App < Sinatra::Base
     @phrase = params[:phrase]
     string = ""
     @num.times {string += "#{phrase}"}
+    string
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
