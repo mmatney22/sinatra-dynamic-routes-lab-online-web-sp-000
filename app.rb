43d86bb8cdd5602166ee50cadddfe0cd0b24490e
Morgan Matney <morgan.matney89@gmail.com>
2020-11-17 13:51:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,13 +31,13 @@ class App < Sinatra::Base
     @num1 = params[:number1]
     @num2 = params[:number2]
     if @operation == "add"
-      "#{num1 + num2}"
+      "#{@num1 + @num2}"
     elsif @operation == "subtract"
-      "#{num1 - num2}"
+      "#{@num1 - @num2}"
     elsif @operation == "multiply"
-      "#{num1 * num2}"
+      "#{@num1 * @num2}"
     elsif @operation == "divide"
-      "#{num1 / num2}"
+      "#{@num1 / @num2}"
     end
   end
 end
